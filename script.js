61b4a5c0a2a13a19dcac585434fae6ead43a39a8
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-18 10:33:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 function toFahrenheit(celsius) {
-    // Calculate Fahrenheit using the conversion formula
     let fahrenheit = (celsius * 9 / 5) + 32;
-    
-    // Round to two decimal places
     fahrenheit = fahrenheit.toFixed(2);
-    
-    // Return the result as a number (not a string)
     return parseFloat(fahrenheit);
 }
-
-// Prompt the user for input and display the result
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
